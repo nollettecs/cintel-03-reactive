8995d8c5c03f3ad63356dc2fa27c037395885640
Cole Nollette <155999592+nollettecs@users.noreply.github.com>
2024-03-18 10:08:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -116,3 +116,16 @@ with ui.navset_card_tab(id="tab"):
                      'Chinstrap': 'green',
                      'Gentoo': 'red'},
             )
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
